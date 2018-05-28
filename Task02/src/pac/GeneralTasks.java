29ffd7fb408d5343e3b9e7553c16e969ba53f975
Ksenia <ksundelok@gmail.com>
2018-05-28 22:36:26 +0300

    correct mistakes task2

--- Task02/src/pac/GeneralTasks.java
+++ Task02/src/pac/GeneralTasks.java
@@ -4,50 +4,62 @@ class GeneralTasks {
     private final static int TEN = 10;
 
     //task1
-    public static void theSame(int a, int b, int c) {
-        System.out.println("a = b = c = " + ((a == b) && (b == c)));
+    public static boolean theSame(int a, int b, int c) {
+        return (a == b) && (b == c);
     }
 
     //task2
-    public static void weihtOfDinasaur(int weight) {
-        int tmp;
-        tmp = weight * 1000;
-        System.out.println("In gramm: " + tmp);
-        tmp *= 1000;
-        System.out.println("In milligram: " + tmp);
-        tmp = weight / 1000;
-        System.out.println("In tonn: " + tmp);
+    private  final static int GRAMM = 1000;
+    private  final static int MIGRAMM = 1000000;
+    private  final static int TONE = 1000;
+    public static int convertKgIntoGramm(int weight) {
+       return weight * GRAMM;
+    }
+    public static int convertKgIntoMiGramm(int weight) {
+        return weight * MIGRAMM;
+    }
+    public static int convertKgIntoTon(int weight) {
+        return weight/ TONE;
     }
 
+
     //task3
-    public static void squareOfRing(int r1, int r2) {
-        double square = Math.abs(Math.PI * Math.pow(r1, 2) - Math.PI * Math.pow(r2, 2));
-        System.out.println("Square of ring: " + square);
+    public static double calcsquareOfCircle(int r1){
+        return Math.PI * Math.pow(r1, 2);
+    }
+    public static double calcsquareOfRing(int r1, int r2) {
+       return Math.abs(calcsquareOfCircle(r1) - calcsquareOfCircle(r2));
     }
 
     //task5
-    public static void arifmeticAndGeometric(int a) {
-        double sum = a % TEN;
+    public static double calcaGeometric(int a) {
         double mul = a % TEN;
         a /= TEN;
-        sum += a % TEN;
         mul *= a % TEN;
         a /= TEN;
-        sum += a % TEN;
         mul *= a % TEN;
         a /= TEN;
-        sum += a % TEN;
         mul *= a % TEN;
         a /= TEN;
-        sum += a % TEN;
         mul *= a % TEN;
         a /= TEN;
-        sum += a % TEN;
         mul *= a % TEN;
+      return Math.sqrt(mul);
+    }
+    public static double calcArifmetic(int a) {
+        double sum = a % TEN;
+        a /= TEN;
+        sum += a % TEN;
+        a /= TEN;
+        sum += a % TEN;
+        a /= TEN;
+        sum += a % TEN;
+        a /= TEN;
+        sum += a % TEN;
+        a /= TEN;
+        sum += a % TEN;
         sum /= 6;
-        mul = Math.sqrt(mul);
-        System.out.println("Arifmetic average: " + sum);
-        System.out.println("Geometric average: " + mul);
+        return sum;
     }
 
     //task4
@@ -72,7 +84,7 @@ class GeneralTasks {
 
 
     //task6
-    public static void reversNumber(int a) {
+    public static int reversNumber(int a) {
         int num = a % TEN;
         a /= TEN;
         int res = num;
@@ -94,15 +106,13 @@ class GeneralTasks {
         num = a % TEN;
         a /= TEN;
         res = res * TEN + num;
-        System.out.println(res);
+      return res;
     }
 
     //task7
     public static void swapWithoutVar(int a, int b) {
-        System.out.println("Before: " + "a = " + a + " " + "b = " + b);
         a += b;
         b = a - b;
         a -= b;
-        System.out.println("After: " + "a = " + a + ' ' + "b = " + b);
     }
 }
--- Task02/src/pac/Task02.java
+++ Task02/src/pac/Task02.java
@@ -3,10 +3,10 @@ package pac;
 public class Task02 {
     public static void main(String[] args) {
         System.out.println(GeneralTasks.sequence(12345));
-        GeneralTasks.arifmeticAndGeometric(1212);
-        GeneralTasks.weihtOfDinasaur(123);
+        GeneralTasks.calcaGeometric(1212);
+        GeneralTasks.calcArifmetic(1234);
         GeneralTasks.reversNumber(123456);
-        GeneralTasks.squareOfRing(1, 1);
+        GeneralTasks.calcsquareOfRing(1, 1);
         GeneralTasks.swapWithoutVar(3, 4);
         GeneralTasks.theSame(1, 1, 1);
     }
