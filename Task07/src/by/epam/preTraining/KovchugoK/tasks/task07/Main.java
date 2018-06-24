bb0a76f03cd55ec99e1cd7ed80ff8104288e216e
Ksenia <ksundelok@gmail.com>
2018-06-24 12:29:26 +0300

    add sorter

--- Task07/src/by/epam/preTraining/KovchugoK/tasks/task07/Main.java
+++ Task07/src/by/epam/preTraining/KovchugoK/tasks/task07/Main.java
@@ -13,18 +13,17 @@ public class Main {
         Car[] cars = new Car[4];
         cars[0] = new Car(1994, 10000, "Honda", "Hatchback");
         cars[1] = new Car(1994, 500000, "Lexus", "Sedan");
-        cars[2] = new Car(1995, 500, "Lada", "Sedan");
+       cars[2] = new Car(1995, 500, "Lada", "Sedan");
         cars[3] = new Car(1994, 145880, "Lexus", "Hatchback");
         Taxi taxi = new Taxi(12, cars.length, cars);
-        Sorter.sortByPrice(taxi.getCars());
-        System.out.println(taxi.toString());
+        Sorter.sortByPrice2(taxi.getCars());
+         System.out.println(taxi.toString());
        TaxiCabby taxiCabby = new TaxiCabby(taxi);
         taxiCabby.addCar(new Car(1999,2899, "Ferrary", "Sedan"));
         System.out.println(taxiCabby.toString());
         taxiCabby.deleteCarByIndex(1);
         System.out.println(taxiCabby.toString());
-        System.out.println(Arrays.toString(taxiCabby.findByCarModelType("Sedan")));
-        System.out.println(taxiCabby.getByIndex(0));
+        
 
     }
 }
--- Task07/src/by/epam/preTraining/KovchugoK/tasks/task07/utils/Sorter.java
+++ Task07/src/by/epam/preTraining/KovchugoK/tasks/task07/utils/Sorter.java
@@ -5,14 +5,32 @@ import by.epam.preTraining.KovchugoK.tasks.task07.model.Taxi;
 import by.epam.preTraining.KovchugoK.tasks.task07.model.Vehicle;
 
 public class Sorter {
-    public static void sortByPrice(Car[] cars) {
+    public static void sortByPrice1(Car[] cars) {
         for (int i = 0; i < cars.length - 1; i++) {
-            for (int j = 0; j < cars.length - 1 ; j++) {
-                if(cars[j].getPrice() > cars[j+1].getPrice()){
+            for (int j = 0; j < cars.length - 1; j++) {
+                if (cars[j].getPrice() > cars[j + 1].getPrice()) {
                     Car temp = cars[j];
                     cars[j] = cars[j + 1];
                     cars[j + 1] = temp;
                 }
+            }
+        }
+    }
+//selection sort
+    public static void sortByPrice2(Car[] cars) {
+        for (int i = 0; i < cars.length; i++) {
+            double min = cars[i].getPrice();
+            int min_i = i;
+            for (int j = i + 1; j < cars.length; j++) {
+                if (cars[j].getPrice() < min) {
+                    min = cars[j].getPrice();
+                    min_i = j;
+                }
+            }
+            if (i != min_i) {
+                Car temp = cars[i];
+                cars[i] = cars[min_i];
+                cars[min_i] = temp;
             }
         }
     }
